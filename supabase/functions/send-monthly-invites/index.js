4be0ea5f589e1267b64000eb5cb89916009b0f15
Himavarshith <hima16249@gmail.com>
2026-03-10 18:33:10 +0530

    cors

--- supabase/functions/send-monthly-invites/index.js
+++ supabase/functions/send-monthly-invites/index.js
@@ -3,7 +3,8 @@ import { sendMonthlyInvites } from "../waitlist-engine/index.js";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers":
+    "Content-Type, Authorization, apikey , x-client-info",
 };
 
 Deno.serve(async (req) => {
